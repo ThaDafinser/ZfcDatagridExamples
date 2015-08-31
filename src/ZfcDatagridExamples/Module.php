310cb39fb04d1aed9d7190b19e165f68ae85e70b
Martin Keckeis <martin.keckeis1@gmail.com>
2015-08-31 15:07:23 +0200

    remove old autoloader from ZF2 and support composer only

--- src/ZfcDatagridExamples/Module.php
+++ src/ZfcDatagridExamples/Module.php
@@ -6,21 +6,6 @@ use Zend\Console\Adapter\AdapterInterface as Console;
 class Module
 {
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/../../autoload_classmap.php'
-            ),
-            
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
-                )
-            )
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
